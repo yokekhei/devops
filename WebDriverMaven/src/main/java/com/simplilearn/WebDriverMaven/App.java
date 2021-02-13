bf13bb62aecb9f85ac9c48c77374c0b8ea34d313
yokekhei <yokekhei@yahoo.com>
2021-02-13 18:05:14 +0000

    WebDriver maven project

---
+++ WebDriverMaven/src/main/java/com/simplilearn/WebDriverMaven/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.WebDriverMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ WebDriverMaven/src/test/java/com/simplilearn/WebDriverMaven/AppTest.java
@@ -0,0 +1,88 @@
+package com.simplilearn.WebDriverMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+	private WebDriver driver;
+	
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.simplilearn.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+	}
+	
+	public void login() {
+		WebElement lnkLogin = driver.findElement(By.className("login"));
+		lnkLogin.click();
+		
+		WebElement editUsername = driver.findElement(By.name("user_login"));
+		editUsername.sendKeys("xyz@abc.com");
+		
+		WebElement editPwd = driver.findElement(By.name("user_pwd"));
+		editPwd.sendKeys("12345");
+		
+		WebElement btnLogin = driver.findElement(By.name("btn_login"));
+		btnLogin.click();
+		
+		WebElement error = driver.findElement(By.id("msg_box"));
+		String actMsg = error.getText();
+		String expMsg = "The email or password you have entered is invalid.";
+		if (actMsg.equals(expMsg)) {
+			System.out.println("TC Passed");
+		} else {
+			System.out.println("TC Failed");
+		}
+	}
+	
+	public void teardown() {
+		driver.quit();
+	}
+	
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	setup();
+    	login();
+    	teardown();
+        assertTrue( true );
+    }
+}
