674d3ae943646707f25ac293cb3f85ca00bedcd9
yokekhei <yokekhei@yahoo.com>
2021-02-21 16:07:25 +0000

    Parameter and ApachePOI examples

--- SimpliLearn/src/test/java/com/simplilearn/SimpliLearn/ApachePOI.java
+++ SimpliLearn/src/test/java/com/simplilearn/SimpliLearn/ApachePOI.java
@@ -1,9 +1,14 @@
 package com.simplilearn.SimpliLearn;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,12 +17,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
-public class LoginTest {
+public class ApachePOI {
 
 	private WebDriver driver;
 	private SoftAssert assetobj;
+	
+	private XSSFWorkbook wb;
+	XSSFSheet sheet1; 
 	
 	@BeforeTest
 	public void setup() {
@@ -32,18 +39,36 @@ public class LoginTest {
 	}
 
 	@Test
-	@Parameters({"UserName", "Password"})
-	public void TestCase1(String UserName, String Password) {
+	public void TestCase1() throws IOException {
+		FileInputStream fis = new FileInputStream("exceldata.xlsx");
+		wb = new XSSFWorkbook(fis);
+		sheet1 = wb.getSheet("data");
+		
+//		String username = sheet1.getRow(0).getCell(0).getStringCellValue();
+//		String password = sheet1.getRow(0).getCell(1).getStringCellValue();
+		
+		int rowCount = sheet1.getLastRowNum();
+		String username = null;
+		String password = null;
+		
+		for (int i = 0; i<rowCount; i++) {
+			String testName = sheet1.getRow(i).getCell(0).getStringCellValue();
+			if (testName.equals("mytest")) {
+				username = sheet1.getRow(i).getCell(1).getStringCellValue();
+				password = sheet1.getRow(i).getCell(2).getStringCellValue();
+			}
+		}
+		
 		WebElement element = driver.findElement(By.linkText("Log in"));
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		wait.until(ExpectedConditions.visibilityOf(element));
 		element.click();
 
 		WebElement userid = driver.findElement(By.cssSelector("input.email"));
-		userid.sendKeys(UserName);
+		userid.sendKeys(username);
 
 		WebElement pwd = driver.findElement(By.cssSelector("input[name=user_pwd]"));
-		pwd.sendKeys(Password);
+		pwd.sendKeys(password);
 
 		WebElement button = driver.findElement(By.name("btn_login"));
 		button.click();
